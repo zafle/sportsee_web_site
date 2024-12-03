ff4d5dada932f7a3ea3b64de0453ddfaef34523e
zafle <fleutotanne@free.fr>
2024-12-03 12:37:14 +0100

    add profile header component

---
+++ src/components/ProfileHeader/ProfileHeader.jsx
@@ -0,0 +1,23 @@
+import { useAccount } from '../../hooks/useAccount'
+import { useGetUserInfos } from '../../services/useDataFactory'
+
+export function ProfileHeader() {
+    const { userId } = useAccount()
+    const { isLoading, data, error } = useGetUserInfos(userId)
+    const userInfos = data ? data : null
+
+    if (error) {
+        return (
+            <span>
+                Désolés, nous rencontrons un problème avec le chargement des
+                données.
+            </span>
+        )
+    }
+
+    return isLoading ? (
+        <div>chargement...</div>
+    ) : (
+        <div>Bonjour {userInfos?.firstName}</div>
+    )
+}
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,28 +1,10 @@
-import { useAccount } from '../../hooks/useAccount'
-import { useGetUserInfos } from '../../services/useDataFactory'
+import { ProfileHeader } from '../../components/ProfileHeader/ProfileHeader'
 
 /**
  * Component to display Profil Page content
  * @returns {JSX.Element}
  */
 function Profile() {
-    const { userId } = useAccount()
-    const { isLoading, data, error } = useGetUserInfos(userId)
-    const userInfos = data ? data : null
-
-    if (error) {
-        return (
-            <span>
-                Désolés, nous rencontrons un problème avec le chargement des
-                données.
-            </span>
-        )
-    }
-
-    return isLoading ? (
-        <div>chargement...</div>
-    ) : (
-        <div>Bonjour {userInfos?.firstName}</div>
-    )
+    return <ProfileHeader />
 }
 export default Profile
